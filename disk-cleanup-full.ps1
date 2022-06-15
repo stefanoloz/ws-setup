d7912c6d6fba5b05849d8a47235f15576e71e519
stefanoloz <82564531+stefanoloz@users.noreply.github.com>
2022-06-15 16:23:43 +0200

    Add files via upload

---
+++ disk-cleanup-full.ps1
@@ -0,0 +1,10 @@
+﻿.\Set-SageSet1.ps1
+
+start-process -Wait -FilePath C:\Windows\System32\cleanmgr.exe /sagerun:1
+
+start-process -Wait -FilePath "C:\Windows\System32\dism.exe" -ArgumentList "/Online /Cleanup-Image /AnalyzeComponentStore"
+start-process -Wait -FilePath "C:\Windows\System32\dism.exe" -ArgumentList " /Online /Cleanup-Image /StartComponentCleanup"
+start-process -Wait -FilePath "C:\Windows\System32\Dism.exe" -ArgumentList " /online /Cleanup-Image /StartComponentCleanup /resetbase"
+start-process -Wait -FilePath "C:\Windows\System32\Dism.exe" -ArgumentList " /Online /Cleanup-Image /SPSuperseded"
+
+Optimize-Volume -DriveLetter C -defrag -Verbose
